29e7bb88d725791d16cc0afc2255697e3e0063ed
Oleksandr Bortnyk <oleksandr.bortnyk@vimmi.net>
2021-08-24 02:49:19 +0300

    configuring of drawer and navigation components

--- app/src/main/java/com/android/academy/mentorpetproject/MainActivity.kt
+++ app/src/main/java/com/android/academy/mentorpetproject/MainActivity.kt
@@ -2,10 +2,41 @@ package com.android.academy.mentorpetproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.onNavDestinationSelected
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navigationView: NavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navController = findNavController(R.id.fragment_container)
+        drawerLayout = findViewById(R.id.drawer_layout)
+        navigationView = findViewById(R.id.nav_view)
+        navigationView.setupWithNavController(navController)
+        
+        appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+
+
+        /**
+         * Sets up a [NavigationView] for use with a [NavController]. This will call
+         * [android.view.MenuItem.onNavDestinationSelected] when a menu item is selected.
+         * The selected item in the NavigationView will automatically be updated when the destination
+         * changes.
+         */
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
 }
