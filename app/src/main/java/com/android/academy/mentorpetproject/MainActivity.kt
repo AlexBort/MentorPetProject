a05aae97909822c86e322d39c87ca3ad7545474b
Oleksandr Bortnyk <oleksandr_bortnyk@epam.com>
2021-09-05 20:20:52 +0300

    deleted unneeded code

--- app/src/main/java/com/android/academy/mentorpetproject/MainActivity.kt
+++ app/src/main/java/com/android/academy/mentorpetproject/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.android.academy.mentorpetproject
 
-import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.drawerlayout.widget.DrawerLayout
@@ -16,8 +15,6 @@ class MainActivity : AppCompatActivity() {
     private lateinit var drawerLayout: DrawerLayout
     private lateinit var navigationView: NavigationView
 
-    private lateinit var listener: NavController.OnDestinationChangedListener
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -28,42 +25,18 @@ class MainActivity : AppCompatActivity() {
         navigationView.setupWithNavController(navController)
 
 
-        val topLevelDestinations: Set<Int> = hashSetOf(R.id.firstFragment,R.id.secondFragment,R.id.profileFragment)
-appBarConfiguration = AppBarConfiguration.Builder(topLevelDestinations).setOpenableLayout(drawerLayout).build()
-//        appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+        val topLevelDestinations: Set<Int> =
+            hashSetOf(R.id.firstFragment, R.id.secondFragment, R.id.profileFragment)
+        appBarConfiguration =
+            AppBarConfiguration.Builder(topLevelDestinations).setOpenableLayout(drawerLayout)
+                .build()
 
-        /**
-         * Sets up a [NavigationView] for use with a [NavController]. This will call
-         * [android.view.MenuItem.onNavDestinationSelected] when a menu item is selected.
-         * The selected item in the NavigationView will automatically be updated when the destination
-         * changes.
-         */
-//        setupActionBarWithNavController(navController, appBarConfiguration)
         NavigationUI.setupActionBarWithNavController(this,
             this.navController,
             this.appBarConfiguration)
-
-/*        // example of using controllerListener
-        listener = NavController.OnDestinationChangedListener { controller, destination, arguments ->
-                if (destination.id == R.id.firstFragment) {
-                    supportActionBar?.setDisplayHomeAsUpEnabled(false)
-//                    supportActionBar?.setBackgroundDrawable(ColorDrawable(getColor(R.color.purple_700)))
-                } else if (destination.id == R.id.secondFragment) {
-                    supportActionBar?.setDisplayHomeAsUpEnabled(false)
-//                    supportActionBar?.setBackgroundDrawable(ColorDrawable(getColor(R.color.black)))
-                } else if (destination.id == R.id.profileFragment) {
-                    supportActionBar?.setDisplayHomeAsUpEnabled(true)
-                }
-            }*/
-    }
-
-    override fun onResume() {
-        super.onResume()
-//        navController.addOnDestinationChangedListener(listener)
     }
 
     override fun onSupportNavigateUp(): Boolean {
-//        val navController = findNavController(R.id.fragment_container)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 
